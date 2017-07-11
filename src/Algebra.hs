15dcf52d30e0817788a113a4654d7374e868f5b5
Aleksandr Rodin <alerodin@gmail.com>
2017-07-11 10:06:11 +0800

    issue resolving

---
+++ src/Algebra.hs
@@ -0,0 +1,132 @@
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
+
+module Algebra where
+
+import           Data.Biapplicative
+import           Protolude
+
+-- AUXILIARY TYPES --
+
+type Scalar = Complex Double
+
+infixr 8 :|:
+data a :|: b = a :|: b deriving (Show)
+
+instance Bifunctor (:|:) where
+  bimap f g (a :|: b) = f a :|: g b
+
+instance Biapplicative (:|:) where
+  bipure = (:|:)
+  (<<*>>) (f:|:g) = bimap f g
+
+-- TYPECLASSES --
+
+class QuantumBasis a where
+  mkState :: a -> QuantumState a
+  infixr 7 |.|
+  (|.|) :: QuantumState a -> QuantumState a -> Scalar
+
+class QuantumUnit a where
+  type Basis a
+  infixl 6 |+|
+  (|+|) :: a -> a -> a
+  infixl 6 |-|
+  (|-|) :: a -> a -> a
+  infixr 7 |*|
+  (|*|) :: Scalar -> a -> a
+  hC :: a -> a
+
+class QuantumApplication a b where
+  infixr 6 |->|
+  (|->|) :: a -> b -> b
+  infixl 6 |<-|
+  (|<-|) :: b -> a -> b
+
+-- DEFINITION OF QUANTUM STATE --
+
+newtype QuantumState a = QuantumState {qState :: a -> Scalar}
+
+instance (QuantumBasis a) => QuantumUnit (QuantumState a) where
+  type Basis (QuantumState a) = a
+  (|+|) qS1 qS2 = QuantumState {qState = \x -> qState qS1 x + qState qS2 x}
+  (|-|) qS1 qS2 = QuantumState {qState = \x -> qState qS1 x - qState qS2 x}
+  (|*|) scalar qS = QuantumState {qState = (scalar *) <$> qState qS}
+  hC qS = QuantumState {qState = conjugate <$> qState qS}
+
+infixr 7 |<>|
+(|<>|) :: (QuantumBasis a) => QuantumState a -> QuantumState a -> Scalar
+(|<>|) l r = hC l |.| r
+
+infixr 7 |><|
+(|><|) :: (QuantumBasis a) => QuantumState a -> QuantumState a -> QuantumOperator a
+(|><|) l r = QuantumOperator{
+              operator = \x -> (r |<>| mkState x) |*| l
+            , operatorHC = \x -> (l |<>| mkState x) |*| r}
+
+infixr 7 |>|
+(|>|) :: (QuantumBasis a) => a -> QuantumState a -> Scalar
+(|>|) st qS = qState qS st
+
+infixr 7 |<|
+(|<|) :: (QuantumBasis a) => QuantumState a -> a -> Scalar
+(|<|) qS st = conjugate $ qState qS st
+
+normalize :: (QuantumBasis a) => QuantumState a -> QuantumState a
+normalize qS = (1/sqrt( qS |<>| qS )) |*| qS
+
+-- DEFINITION OF QUANTUM OPERATOR --
+
+data QuantumOperator a = QuantumOperator {
+                          operator   :: a -> QuantumState a
+                        , operatorHC :: a -> QuantumState a}
+
+instance (QuantumBasis a) => QuantumUnit (QuantumOperator a) where
+    type Basis (QuantumOperator a) = a
+    (|+|) qO1 qO2 = QuantumOperator{
+                    operator = \x -> operator qO1 x |+| operator qO2 x
+                  , operatorHC = \x -> operatorHC qO1 x |+| operatorHC qO2 x}
+    (|-|) qO1 qO2 = QuantumOperator{
+                    operator = \x -> operator qO1 x |-| operator qO2 x
+                  , operatorHC = \x -> operatorHC qO1 x |-| operatorHC qO2 x}
+    (|*|) scalar qO = QuantumOperator {
+                      operator = (scalar |*|) <$> operator qO
+                    , operatorHC = (conjugate scalar |*|) <$> operatorHC qO}
+    hC qO = QuantumOperator {
+            operator = operatorHC qO
+          , operatorHC = operator qO}
+
+infixr 8 |>>|
+(|>>|) :: (QuantumBasis a) => (a -> QuantumState a) -> QuantumState a -> QuantumState a
+(|>>|) f qS = QuantumState {qState = \x -> qS |.| f x}
+
+infixr 9 |:|
+(|:|) :: (QuantumBasis a) => QuantumOperator a -> QuantumOperator a -> QuantumOperator a
+(|:|) qO1 qO2 = QuantumOperator{
+                  operator = \x -> operator qO1 |>>| operator qO2 x
+                , operatorHC = \x -> operatorHC qO2 |>>| operatorHC qO1 x}
+
+instance (QuantumBasis a, a ~ b) => QuantumApplication (QuantumOperator a) (QuantumState b) where
+  (|->|) qO qS = operator qO |>>| qS
+  (|<-|) qS qO = hC (hC qO |->| hC qS)
+
+instance (QuantumBasis a, a ~ b, QuantumApplication c d) => QuantumApplication (QuantumOperator a :|: c) (QuantumState b :|: d) where
+  (|->|) qO qS = bimap (|->|) (|->|) qO Data.Biapplicative.<<*>> qS
+  (|<-|) qS qO = bimap (|<-|) (|<-|) qS Data.Biapplicative.<<*>> qO
+
+-- BASIS CHANGE
+
+-- ----------------
+--
+-- matrixForm :: (Ord a) => QuantumOperator a -> [Ket a] -> [[Amplitude]]
+-- matrixForm op kets =
+--   let
+--     bras = fmap hC kets
+--     elements =  fmap (fmap (|.|) bras <*> ) $ fmap (op |->|) <$> group kets
+--   in
+--     elements
+--
+-- commute :: (Ord a) => QuantumOperator a -> QuantumOperator a -> QuantumOperator a
+-- commute a b = a |:| b |-| b |:| a
--- src/Spin.hs
+++ src/Spin.hs
@@ -1,8 +1,9 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeFamilies      #-}
 
 module Spin where
 
-import           BraKet
+import           Algebra
 import qualified Data.Map  as Map
 import           Protolude
 
@@ -13,12 +14,13 @@ import           Protolude
 data Spin = SpinUp
           | SpinDown deriving (Eq,Show, Ord)
 
-zUp :: Ket Spin
-zUp = mkKet (1 :+ 0) SpinUp
+-- instance QuantumBasis Spin where
+--   mkState s = QuantumState {qState = \x -> fromMaybe (0:+0) $ Map.lookup x $ Map.singleton s (1 :+ 0)}
 
-zDown :: Ket Spin
-zDown = mkKet (1 :+ 0) SpinDown
+-- instance BraKet (QuantumState Spin) where
+--   (|.|) a b = 6
 
+<<<<<<< HEAD
 xUp ::  Ket Spin
 xUp = normalize (zUp |+| zDown)
 
@@ -41,3 +43,46 @@ jz = QuantumKernel { kernel = f }
 
 jzOp :: QuantumOperator Spin
 jzOp = QuantumOperator jz jz
+=======
+-- qS1 :: QuantumState Spin
+-- qS1 x =
+--   case x of
+--     SpinUp   -> 1
+--     SpinDown -> -1
+--
+-- qS2 :: QuantumState Spin
+-- qS2 x =
+--   case x of
+--     SpinUp   -> 3
+--     SpinDown -> -3
+
+--
+-- zUp :: Ket Spin
+-- zUp = mkKet (1 :+ 0) SpinUp
+--
+-- zDown :: Ket Spin
+-- zDown = mkKet (1 :+ 0) SpinDown
+--
+-- xUp ::  Ket Spin
+-- xUp = normalize (zUp |+| zDown)
+--
+-- xDown ::  Ket Spin
+-- xDown = normalize (zUp |-| zDown)
+--
+-- yUp ::  Ket Spin
+-- yUp = normalize (zUp |+| (0 :+ 1 :: Complex Double) |*| zDown)
+--
+-- yDown ::  Ket Spin
+-- yDown = normalize (zUp |-| (0 :+ 1 :: Complex Double) |*| zDown)
+--
+-- jz :: QuantumKernel Spin
+-- jz = QuantumKernel { kernel = f }
+--   where
+--   f x =
+--     case x of
+--       SpinUp   -> Map.singleton SpinUp (0.5 :+ 0)
+--       SpinDown -> Map.singleton SpinDown ((-0.5) :+ 0)
+--
+-- jzOp :: QuantumOperator Spin
+-- jzOp = QuantumOperator jz jz
+>>>>>>> origin/master
